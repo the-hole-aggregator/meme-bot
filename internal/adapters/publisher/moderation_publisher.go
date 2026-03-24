46d1edd5ae86da465d5b7ce5ff5041e4c4bad8c1
Egor Grischenkov <66774252+egrischenkov@users.noreply.github.com>
2026-03-24 20:53:18 +0300

    MEME-15: feat(moderation): implement moderation publisher

---
+++ internal/adapters/publisher/moderation_publisher.go
@@ -0,0 +1,38 @@
+package publisher
+
+import (
+	"fmt"
+	"meme-bot/internal/domain"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type ModerationPublisher struct {
+	bot    *tgbotapi.BotAPI
+	chatID int64
+}
+
+func NewModerationPublisher(bot *tgbotapi.BotAPI, chatID int64) *ModerationPublisher {
+	return &ModerationPublisher{bot: bot, chatID: chatID}
+}
+
+func (p *ModerationPublisher) Publish(meme domain.Meme) error {
+	file := tgbotapi.FilePath(fmt.Sprintf("tmp/%s.jpg", meme.SourceID))
+
+	msg := tgbotapi.NewPhoto(p.chatID, file)
+
+	msg.Caption = fmt.Sprintf("ID: %d", meme.ID)
+
+	msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("👍", fmt.Sprintf("approved:%d", meme.ID)),
+			tgbotapi.NewInlineKeyboardButtonData("👎", fmt.Sprintf("rejected:%d", meme.ID)),
+		),
+	)
+
+	if _, err := p.bot.Send(msg); err != nil {
+		return err
+	}
+
+	return nil
+}
